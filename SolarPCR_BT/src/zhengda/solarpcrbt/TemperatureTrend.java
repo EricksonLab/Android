7990765f4baaf80b937d530c622c735045183079
Lexandar <zl259@cornell.edu>
2014-09-29 18:07:43 -0400

    android

---
+++ SolarPCR_BT/src/zhengda/solarpcrbt/TemperatureTrend.java
@@ -0,0 +1,98 @@
+package zhengda.solarpcrbt;
+
+import zhengda.solarpcrbt.R;
+import zhengda.solarpcrbt.R.id;
+import zhengda.solarpcrbt.R.layout;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class TemperatureTrend extends Activity {
+	
+	final private String Tag = "TemperatureTrend";
+	final private String dm_Cel="'C";
+
+	private Button mZoomIn,mZoomOut,mMoveLeft,mMoveRight;
+	static public TextView mTemp0,mTemp1,mTemp2;
+	static public ImageView inRange0,inRange1,inRange2;
+//	private TempTrndGraphView mTempTrndGraphView;
+	private ToggleButton mLocker;
+	private ImageView mCheck0,mCheck1,mCheck2;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.temp_trend);
+		mTemp0 = (TextView) findViewById(R.id.temp0);
+		mTemp1 = (TextView) findViewById(R.id.temp1);
+		mTemp2 = (TextView) findViewById(R.id.temp2);
+		
+		mCheck0 = (ImageView) findViewById(R.id.in_range0);
+		mCheck1 = (ImageView) findViewById(R.id.in_range1);
+		mCheck2 = (ImageView) findViewById(R.id.in_range2);
+		/**
+		Intent fromMain;
+		fromMain = this.getIntent();
+		Log.d(Tag, "Temperature Trend onCreate");
+		Bundle bundle=fromMain.getExtras();
+		
+		if (bundle!=null && bundle.containsKey("Time&Temp")){
+			int t[] = bundle.getIntArray("Time&Temp");
+			mTemp0.setText(""+t[1]);
+			mTemp1.setText(""+t[2]);
+			mTemp2.setText(""+t[3]);
+			Log.d(Tag, "At "+t[0]+" get "+t[1]+" "+t[2]+" "+t[3]);
+		}**/
+		
+	}	
+	
+	@Override
+	protected void onNewIntent(Intent fromMain){
+		super.onNewIntent(fromMain);
+	//	Log.d(Tag, "OnNewIntent");
+		setIntent(fromMain);
+		Bundle bundle=fromMain.getExtras();
+		if (bundle!=null && bundle.containsKey("Time") && bundle.containsKey("Temps")){
+			int time = bundle.getInt("Time");
+			int rawTemps[]=bundle.getIntArray("Temps");
+			float temps[]=new float[rawTemps.length];
+			for (int i=0; i<temps.length;i++)
+				temps[i]=(float)rawTemps[i]/4.0f;
+			if (temps[0]<0.25f)
+				mTemp0.setText("Amp Error");
+			else if (temps[0]>2000f)
+				mTemp0.setText("TC Error");
+			else mTemp0.setText(String.format("%.2f", temps[0])+dm_Cel);
+			if (temps[1]<0.25f)
+				mTemp1.setText("Amp Error");
+			else if (temps[1]>2000f)
+				mTemp1.setText("TC Error");
+			else mTemp1.setText(String.format("%.2f", temps[1])+dm_Cel);
+			if (temps[2]<0.25f)
+				mTemp2.setText("Amp Error");
+			else if (temps[2]>2000f)
+				mTemp2.setText("TC Error");
+			else mTemp2.setText(String.format("%.2f", temps[2])+dm_Cel);
+			//If temps in range, change checks
+			if (temps[0]>=ValueSet.DENATURATION_MIN && temps[0]<=ValueSet.DENATURATION_MAX)
+				mCheck0.setImageResource(R.drawable.ic_check);
+			else mCheck0.setImageResource(R.drawable.blank);
+			
+			if (temps[1]>=ValueSet.EXTENSION_MIN && temps[1]<=ValueSet.EXTENSION_MAX)
+				mCheck1.setImageResource(R.drawable.ic_check);
+			else mCheck1.setImageResource(R.drawable.blank);
+			
+			if (temps[2]>=ValueSet.ANEALING_MIN && temps[2]<=ValueSet.ANEALING_MAX)
+				mCheck2.setImageResource(R.drawable.ic_check);
+			else mCheck2.setImageResource(R.drawable.blank);
+		}
+	}
+}
+
+
